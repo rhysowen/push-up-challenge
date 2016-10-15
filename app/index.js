d96f1098d036f175f6ba5676fe114770d3d9876b
Rhys Owen <contact@rhysowen.me>
2016-10-15 02:55:29 +0100

    Path correction

--- app/index.js
+++ app/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { AppRegistry } from 'react-native';
 import { Provider } from 'react-redux';
 import {
   createStore,
@@ -8,8 +7,8 @@ import {
 } from 'redux';
 import thunkMiddleMiddleware from 'redux-thunk';
 import createLogger from 'redux-logger';
-import reducer from './app/reducers';
-import AppContainer from './app/containers/AppContainer';
+import reducer from './reducers';
+import AppContainer from './containers/AppContainer';
 
 // Only log when node constant __DEV__ is active.
 const loggerMiddleware = createLogger({predicate: (getState, action) => __DEV__});
@@ -27,7 +26,7 @@ function configureStore(initialState) {
 
 const store = configureStore({});
 
-const App = () => (
+export default () => (
   <Provider store={store}>
     <AppContainer />
   </Provider>
