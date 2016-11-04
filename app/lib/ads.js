486b9d4dd8d784c5feb105418ed507c3c5914937
Rhys Owen <contact@rhysowen.me>
2016-11-04 19:11:33 +0000

    No need to remove program

--- app/lib/ads.js
+++ app/lib/ads.js
@@ -1,7 +1,7 @@
 import { AdMobInterstitial } from 'react-native-admob';
 
-export const INTERSTITIAL_AD_UNIT_ID = ''; //'ca-app-pub-6787064558849209/5649878717';
-export const BANNER_AD_UNIT_ID = ''; //ca-app-pub-6787064558849209/2637536714';
+export const INTERSTITIAL_AD_UNIT_ID = 'ca-app-pub-6787064558849209/5649878717';
+export const BANNER_AD_UNIT_ID = 'ca-app-pub-6787064558849209/2637536714';
 export const TEST_DEVICE_ID = 'EMULATOR';
 export const SMART_BANNER_HEIGHT = 50;
 
