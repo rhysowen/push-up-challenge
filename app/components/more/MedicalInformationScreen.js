139a9acbc31e7df47c4a0875e5939e6d2e32e346
Rhys Owen <contact@rhysowen.me>
2016-11-06 22:58:35 +0000

    Medical Information

--- app/components/more/MedicalInformationScreen.js
+++ app/components/more/MedicalInformationScreen.js
@@ -2,15 +2,11 @@ import React from 'react';
 
 import Content from '../shared/Content';
 
-const ADVISORY_HEADER = 'Advisory';
-const ADVISORY_CONTENT = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
+const ADVISORY_HEADER = 'Disclaimer';
+const ADVISORY_CONTENT = 'Before beginning any exercise program, you should consult your physician.';
 
 export default () => (
   <Content>
-    <Content.Item
-      header={ADVISORY_HEADER}
-      content={ADVISORY_CONTENT}
-    />
     <Content.Item
       header={ADVISORY_HEADER}
       content={ADVISORY_CONTENT}
