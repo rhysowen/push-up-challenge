719c6299382aa38dc614233c97d048f295659da5
Rhys Owen <contact@rhysowen.me>
2016-11-06 23:01:07 +0000

    Advisory => Disclaimer

--- app/components/more/MedicalInformationScreen.js
+++ app/components/more/MedicalInformationScreen.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 import Content from '../shared/Content';
 
-const ADVISORY_HEADER = 'Disclaimer';
-const ADVISORY_CONTENT = 'Before beginning any exercise program, you should consult your physician.';
+const DISCLAIMER_HEADER = 'Disclaimer';
+const DISCLAIMER_CONTENT = 'Before beginning any exercise program, you should consult your physician.';
 
 export default () => (
   <Content>
     <Content.Item
-      header={ADVISORY_HEADER}
-      content={ADVISORY_CONTENT}
+      header={DISCLAIMER_HEADER}
+      content={DISCLAIMER_CONTENT}
     />
   </Content>
 );
