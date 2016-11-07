288658a669199d77cb219607c00c13543026c4cc
Rhys Owen <contact@rhysowen.me>
2016-11-07 23:25:56 +0000

    Provide linking for iOS & Android

---
+++ app/lib/linking.js
@@ -0,0 +1,12 @@
+import { Linking } from 'react-native';
+
+export default url => (
+  Linking.canOpenURL(url).then((supported) => {
+    debugger;
+    if (supported) {
+      return Linking.openURL(url);
+    }
+
+    return -1;
+  }).catch(err => console.error('An error occurred', err))
+);
